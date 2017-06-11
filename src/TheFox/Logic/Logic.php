ce5a637ec7f9ad4eb2257969bec7c3a85d87d24c
Christian Mayer <christian@fox21.at>
2017-06-11 16:31:33 +0200

    Initial commit. #RN

---
+++ src/TheFox/Logic/Logic.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TheFox\Logic;
+
+class Logic
+{
+    const NAME = 'Logic';
+    const VERSION = '0.1.0-dev.1';
+}
---
+++ src/TheFox/Logic/OrGate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TheFox\Logic;
+
+class OrGate extends Gate
+{
+    /**
+     * @return bool
+     */
+    public function getBool(): bool
+    {
+        if ($this->getObj1() && $this->getObj1()->getBool() || $this->getObj2() && $this->getObj2()->getBool()) {
+            return true;
+        }
+        return false;
+    }
+}
